e9c224a0a87617492a364f80396c60bac72dd513
erik1110 <oceanuheart@gmail.com>
2023-04-20 08:51:42 +0800

    [feat] add sign-in swagger

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* 登入 */
+router.post('/sign-in', function(req, res, next) {
+    /**
+      * #swagger.tags = ['Login']
+      * #swagger.summary = '登入'
+    */
+    /**
+    #swagger.parameters['parameter_name'] = {
+      in: 'body',
+      description: '登入資料',
+      schema: {
+        $email: 'test@gmail.com',
+        $phone: '0912345678',
+        $first_name: '王',
+        $last_name: '小明',
+        "nick_name": "Wang",
+        $password: 'a1234567',
+        "confirm_password": "a1234567"
+      }
+    }
+    */
+  /**
+    #swagger.responses[201] = {
+      description: '登入成功',
+      schema: { $ref: '#/definitions/Sign' }
+    }
+    #swagger.responses[400] = {
+      description: '登入失敗',
+      schema: { $ref: '#/definitions/Error' }
+    }
+    #swagger.responses[404] = {
+      description: '無此路由',
+      schema: { $ref: '#/definitions/Error' }
+    }
+    #swagger.responses[500] = {
+      description: '系統錯誤',
+      schema: { $ref: '#/definitions/Error' }
+    }
+  */
   res.send('respond with a resource');
 });
 
