f74b406b7e6dd8b8e7d647fdd60ec41760ad70bd
ColdingPoTaTo <william.9150@gmail.com>
2023-04-22 13:52:14 +0800

    create account.js

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors');
-const notFound = require('./middleware/notFound');
-const resError = require('./middleware/resError');
-const swaggerUI = require('swagger-ui-express');
-const swaggerFile = require('./swagger-output.json');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const notFound = require("./middleware/notFound");
+const resError = require("./middleware/resError");
+const swaggerUI = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var postsRouter = require('./routes/posts');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var postsRouter = require("./routes/posts");
+var accountRouter = require("./routes/account");
 
 var app = express();
 
-require('./connections');
+require("./connections");
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', usersRouter);
-app.use('/posts', postsRouter);
-app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerFile))
+app.use("/", usersRouter);
+app.use("/account", accountRouter);
+app.use("/posts", postsRouter);
+app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 // 錯誤管理
 require("./service/process");
