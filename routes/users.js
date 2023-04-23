7cf2735d6907990d83ca20d9144bd8f3610f6309
erik1110 <oceanuheart@gmail.com>
2023-04-23 11:49:50 +0800

    [feat] update swagger

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,15 @@ var router = express.Router();
 const UserController = require('../controller/userController');
 
 /* 登入 */
-router.post('/sign-in', function(req, res, next) {
+router.post('/sign-up', function(req, res, next) {
     /**
       * #swagger.tags = ['Sign-in']
-      * #swagger.summary = '登入'
+      * #swagger.summary = '註冊會員'
     */
     /**
     #swagger.parameters['parameter_name'] = {
       in: 'body',
-      description: '登入資料',
+      description: 'nickName不是必填，其他都是必填',
       schema: {
         $email: 'test@gmail.com',
         $phone: '0912345678',
@@ -24,21 +24,21 @@ router.post('/sign-in', function(req, res, next) {
     }
     */
   /**
-    #swagger.responses[201] = {
+    #swagger.responses[200] = {
       description: '登入成功',
       schema: { $ref: '#/definitions/Sign' }
     }
     #swagger.responses[400] = {
       description: '登入失敗',
-      schema: { $ref: '#/definitions/Error' }
+      schema: { $ref: '#/definitions/Error400' }
     }
     #swagger.responses[404] = {
       description: '無此路由',
-      schema: { $ref: '#/definitions/Error' }
+      schema: { $ref: '#/definitions/Error404' }
     }
     #swagger.responses[500] = {
       description: '系統錯誤',
-      schema: { $ref: '#/definitions/Error' }
+      schema: { $ref: '#/definitions/Error500' }
     }
   */
   const posts = UserController.signUp(req, res, next);
