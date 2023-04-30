f13484e3b3dba6c6bc929a1e9b31d3b8319b5a87
erik1110 <oceanuheart@gmail.com>
2023-04-30 12:17:55 +0800

    [refactor] remove signUp dev

--- routes/users.js
+++ routes/users.js
@@ -3,48 +3,6 @@ var router = express.Router();
 const UserController = require('../controller/userController');
 const { isAuth } = require("../middleware/auth");
 
-/* 註冊 */
-router.post('/sign-up-dev', function(req, res, next) {
-    /**
-      * #swagger.tags = ['Sign-in']
-      * #swagger.summary = 'Register an Account'
-    */
-    /**
-    #swagger.parameters['parameter_name'] = {
-      in: 'body',
-      description: 'nickName is optional, while all the others are required.',
-      schema: {
-        $email: 'test@gmail.com',
-        $phone: '0912345678',
-        $firstName: 'Ho',
-        $lastName: 'Erik',
-        'nickName': 'Erik',
-        $password: 'a1234567',
-        'confirmPassword': "a1234567"
-      }
-    }
-    */
-  /**
-    #swagger.responses[200] = {
-      description: '註冊成功',
-      schema: { $ref: '#/definitions/Sign' }
-    }
-    #swagger.responses[400] = {
-      description: '註冊失敗',
-      schema: { $ref: '#/definitions/Error400' }
-    }
-    #swagger.responses[404] = {
-      description: '無此路由',
-      schema: { $ref: '#/definitions/Error404' }
-    }
-    #swagger.responses[500] = {
-      description: '系統錯誤',
-      schema: { $ref: '#/definitions/Error500' }
-    }
-  */
-  UserController.signUp(req, res, next);
-});
-
 /* 註冊+寄信 */
 router.post('/sign-up', function(req, res, next) {
   /**
@@ -84,7 +42,7 @@ router.post('/sign-up', function(req, res, next) {
     schema: { $ref: '#/definitions/Error500' }
   }
 */
-  UserController.signUpEmail(req, res, next);
+  UserController.signUp(req, res, next);
 });
 
 /* 驗證註冊信 */
