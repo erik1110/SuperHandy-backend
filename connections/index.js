5aa6e6b63932e20ef0c14b8ce8f172d6ce6f41bc
erik1110 <oceanuheart@gmail.com>
2023-05-07 22:19:36 +0800

    [feature] update db

--- connections/index.js
+++ connections/index.js
@@ -6,6 +6,7 @@ const initPlans = require('../db/initPlans');
 const initTasks = require('../db/initTasks');
 const initReviews = require('../db/initReviews');
 const initUsers = require('../db/initUsers');
+const initTransactions = require('../db/initTransactions');
 const initSuperhandyReviews = require('../db/initSuperhandyReviews');
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
@@ -14,11 +15,12 @@ mongoose
     .connect(DB)
     .then(async () => {
         console.log('資料庫連接成功');
-        // await initCategories();
-        // await initPlans();
-        // await initUsers();
-        // await initTasks();
-        // await initReviews();
-        // await initSuperhandyReviews();
+        await initCategories();
+        await initPlans();
+        await initUsers();
+        await initTasks();
+        await initTransactions();
+        await initReviews();
+        await initSuperhandyReviews();
     })
     .catch((err) => console.error('資料庫連接失敗', err));
