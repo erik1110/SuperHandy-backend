c8e06d942fc5261f588db2f9d6de50d3c9311bac
erik1110 <oceanuheart@gmail.com>
2023-05-18 12:07:31 +0800

    [feat] add statusMapping

---
+++ service/statusMapping.js
@@ -0,0 +1,12 @@
+const statusMapping = {
+    draft: '草稿',
+    published: '媒合中',
+    inProgressed: '進行中',
+    submitted: '進行中',
+    confirmed: '已完成',
+    completed: '已完成',
+    unpublished: '已下架',
+    deleted: '未成立',
+};
+
+module.exports = statusMapping;
