2e348c4403a0b3ffc05936b9aefdc5cc2b5cb765
William <william.9150@gmail.com>
2023-06-15 19:00:01 +0800

    [fix] 多寫了驚嘆號

--- controller/findTaskController.js
+++ controller/findTaskController.js
@@ -76,9 +76,9 @@ const tasks = {
                 path: 'reviews',
             });
         let relation = null;
-        if (!userId && task.userId._id.toString() === userId.toString()) {
+        if (userId && task.userId._id.toString() === userId.toString()) {
             relation = 'poster';
-        } else if (!userId && task.helpers && task.helpers.find((helper) => helper.helperId.toString() === userId.toString()) >= 0) {
+        } else if (userId && task.helpers && task.helpers.find((helper) => helper.helperId.toString() === userId.toString()) >= 0) {
             relation = 'helper';
         }
         let reviewRank = {};
